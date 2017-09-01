3695f9448e42f4b15fc1a8cf89b2631feba8664d
Christoph Bernkopf <christoph.bernkopf95@gmail.com>
2017-09-01 13:04:08 +0200

    added sh file for large jobs (event gen.)

---
+++ submitEventGeneration.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+#BSUB -q 1nh
+#
+cd $WORKDIR
+#cd /tmp/cbernkop
+cd /afs/cern.ch/user/c/cbernkop/ComparingTracks/CMSSW_9_3_0_pre1/src/
+cmsenv
+cd $WORKDIR
+#cd /tmp/cbernkop
+cmsRun /afs/cern.ch/user/c/cbernkop/ComparingTracks/CMSSW_9_3_0_pre1/src/10001.0_SingleElectronPt10+SingleElectronPt10_pythia8_2017_GenSimFull+DigiFull_2017+RecoFull_2017+ALCAFull_2017+HARVESTFull_2017/SingleElectronPt10_pythia8_cfi_GEN_SIM.py
+cmsRun /afs/cern.ch/user/c/cbernkop/ComparingTracks/CMSSW_9_3_0_pre1/src/10001.0_SingleElectronPt10+SingleElectronPt10_pythia8_2017_GenSimFull+DigiFull_2017+RecoFull_2017+ALCAFull_2017+HARVESTFull_2017/step2_DIGI_L1_DIGI2RAW_HLT.py
+
+output="step1_`date +%y%m%d%l%M`.root"
+cp step1.root /afs/cern.ch/user/c/cbernkop/ComparingTracks/CMSSW_9_3_0_pre1/src/10001.0_SingleElectronPt10+SingleElectronPt10_pythia8_2017_GenSimFull+DigiFull_2017+RecoFull_2017+ALCAFull_2017+HARVESTFull_2017/$output
+output="step2_`date +%y%m%d%l%M`.root"
+cp step2.root /afs/cern.ch/user/c/cbernkop/ComparingTracks/CMSSW_9_3_0_pre1/src/10001.0_SingleElectronPt10+SingleElectronPt10_pythia8_2017_GenSimFull+DigiFull_2017+RecoFull_2017+ALCAFull_2017+HARVESTFull_2017/$output
+
