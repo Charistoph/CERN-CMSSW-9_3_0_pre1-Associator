94406b18b08dbc6286145508df4518b30f18218f
Christoph Bernkopf <christoph.bernkopf95@gmail.com>
2017-09-14 10:03:14 +0200

    branch init

--- submitEventGeneration.sh
+++ submitEventGeneration.sh
@@ -1,17 +1,18 @@
 #! /bin/bash
 #BSUB -q 1nh
-#
+#echo "Application started"
 cd $WORKDIR
 #cd /tmp/cbernkop
 cd /afs/cern.ch/user/c/cbernkop/ComparingTracks/CMSSW_9_3_0_pre1/src/
 eval `scramv1 runtime -sh`
 cd $WORKDIR
+echo "1 complete"
 #cd /tmp/cbernkop
 cmsRun /afs/cern.ch/user/c/cbernkop/ComparingTracks/CMSSW_9_3_0_pre1/src/10001.0_SingleElectronPt10+SingleElectronPt10_pythia8_2017_GenSimFull+DigiFull_2017+RecoFull_2017+ALCAFull_2017+HARVESTFull_2017/SingleElectronPt10_pythia8_cfi_GEN_SIM.py
 cmsRun /afs/cern.ch/user/c/cbernkop/ComparingTracks/CMSSW_9_3_0_pre1/src/10001.0_SingleElectronPt10+SingleElectronPt10_pythia8_2017_GenSimFull+DigiFull_2017+RecoFull_2017+ALCAFull_2017+HARVESTFull_2017/step2_DIGI_L1_DIGI2RAW_HLT.py
-
-output="step1_`date +%y%m%d%l%M`.root"
+echo "2 complete"
+output="step1_new_`date +%y%m%d%l%M`.root"
 cp step1.root /afs/cern.ch/user/c/cbernkop/ComparingTracks/CMSSW_9_3_0_pre1/src/10001.0_SingleElectronPt10+SingleElectronPt10_pythia8_2017_GenSimFull+DigiFull_2017+RecoFull_2017+ALCAFull_2017+HARVESTFull_2017/$output
-output="step2_`date +%y%m%d%l%M`.root"
+output="step2_new_`date +%y%m%d%l%M`.root"
 cp step2.root /afs/cern.ch/user/c/cbernkop/ComparingTracks/CMSSW_9_3_0_pre1/src/10001.0_SingleElectronPt10+SingleElectronPt10_pythia8_2017_GenSimFull+DigiFull_2017+RecoFull_2017+ALCAFull_2017+HARVESTFull_2017/$output
-
+echo "3 complete"
